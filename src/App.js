01390a5504d12e5101968889fb7ba527603ccb3f
Sylu <mortisflos@gmail.com>
2022-09-20 13:33:11 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react"
+import "./styles/index.css"
+
+const questions = []
+
+function Result(){
+
+}
+
+function Game(){
+
+}
+
+function App(){
+    return (
+        <div className="game">
+
+        </div>
+    )
+
+}
+
+export default App;
+
