b2045ae0301bc539089b46a7b4c5341c46bdda99
Sylu <mortisflos@gmail.com>
2022-09-20 17:00:47 +0300

    add users fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import "./styles/index.css"
+
+export const Users = ({ items, isLoading}) => {
+    
+}
 
 
 function App(){
+    const[users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetch("https://reqres.in/api/users")
+            .then(res => res.json())
+            .then(json => {
+                setUsers(json.data)
+            }).catch(err => {
+                console.warn(err);
+                alert("Ошибка при получении пользователей")
+            })
+    , []})
+
+    return(
+        <div className="App">
+
+        </div>
+    )
 }
 
 export default App;
