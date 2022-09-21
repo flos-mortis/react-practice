a847eda8798326077df77179f3103452d9cb6c8d
Sylu <mortisflos@gmail.com>
2022-09-21 15:44:01 +0300

    add users list render

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react"
 import "./styles/index.css"
+import { Users } from "./components/Users/Users";
 
 function App(){
   const[users, setUsers] = useState([]);
+  const[isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://reqres.in/api/users")
@@ -12,12 +14,12 @@ function App(){
     }).catch(err => {
       console.warn(err);
       alert("Ошибка при получении данных")
-    })
+    }).finally(() => setLoading(false))
   },[])
 
   return (
     <div className="App">
-      <Users items={users}/>
+      <Users items={users} isLoading={isLoading}/>
     </div>
   );
 }
