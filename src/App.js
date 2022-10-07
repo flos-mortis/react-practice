9f46cc5a114dd51cdcb1bf2275408c954c402c9a
Sylu <mortisflos@gmail.com>
2022-10-07 17:59:57 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,61 +1,10 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import "./styles/index.css"
-import { Users } from "./components/Users/Users";
-import { Success } from "./components/Success";
 
 function App(){
-  const[users, setUsers] = useState([]);
-  const[isLoading, setLoading] = useState(true);
-  const[searchValue, setSearchValue] = useState('');
-  const[invites, setInvites] = useState([]);
-  const[success, setSuccess] = useState(false);
-
-  useEffect(() => {
-    fetch("https://reqres.in/api/users")
-    .then(response => response.json())
-    .then(data => {
-      setUsers(data.data)
-    }).catch(err => {
-      console.warn(err);
-      alert("Ошибка при получении данных")
-    }).finally(() => setLoading(false))
-  },[])
-
-  const onChangeSearchValue = (event) => {
-    setSearchValue(event.target.value);
-  }
-
-  const onClickInvite = (id) => {
-    if (invites.includes(id)){
-      setInvites(prev => prev.filter(_id => _id !== id))
-    } else{
-      setInvites(prev => [...prev, id])
-    }
-  }
-
-  const onClickSendInvites = () => {
-    setSuccess(true)
-  }
-  
-  return (
-    <div className="App">
-      {
-        success ? (
-          <Success count={invites.length}/>
-        ) : (
-          <Users 
-            items={users} 
-            isLoading={isLoading} 
-            searchValue={searchValue}
-            onChangeSearchValue={onChangeSearchValue}
-            invites={invites}
-            onClickInvite={onClickInvite}
-            onClickSendInvites={onClickSendInvites}
-          />
-        )
-      }
-    </div>
-  );
+  <div className="App">
+    <p>koko</p>
+  </div>
 }
 
 export default App;
---
+++ src/components/Block.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+const defaultCurrencies = ["RUB", "USD"];
+
+export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => (
+    <div className="block">
+      <p>koko</p>
+    </div>
+  );
